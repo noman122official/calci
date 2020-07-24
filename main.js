d92459b65c6a02f041cbe8d161eaac6c9cb8356f
noman122official <67466365+noman122official@users.noreply.github.com>
2020-07-25 01:43:22 +0530

    Add files via upload
    test ui

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
-function calc() {
-  var a = parseInt(document.querySelector("#value1").value);
-  var b = parseInt(document.querySelector("#value2").value);
-  var operator = document.querySelector("#type").value;
-  var answer;
-
-
-  if (operator == "add"){
-    answer= a + b;
-  }
-  else if (operator == "sub"){
-    answer= a - b;
-  }
-  else if (operator == "mult"){
-    answer= a * b;
-  }
-  else if (operator == "div"){
-    answer= a / b;
-  }
-
-  console.log(answer);
-  document.querySelector("#result").innerHTML = answer;
+//function that display value
+function dis(val)
+{
+    document.getElementById("result").value+=val
 }
+
+//function that evaluates the digit and return result
+function solve()
+{
+    let x = document.getElementById("result").value
+    let y = eval(x)
+    document.getElementById("result").value = y
+}
+
+//function that clear the display
+function clr()
+{
+    document.getElementById("result").value = ""
+} 
