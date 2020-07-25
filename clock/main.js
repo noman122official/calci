03f17a71f625d46484fbf08a4b4dc55df65f916c
noman122official <67466365+noman122official@users.noreply.github.com>
2020-07-26 03:34:55 +0530

    Add files via upload

---
+++ clock/main.js
@@ -0,0 +1,34 @@
+function currentTime() {
+  var date = new Date(); /* creating object of Date class */
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  var midday = "AM";
+  midday = (hour >= 12) ? "PM" : "AM"; /* assigning AM/PM */
+  hour = (hour == 0) ? 12 : ((hour > 12) ? (hour - 12): hour); /* assigning hour in 12-hour format */
+  hour = updateTime(hour);
+  min = updateTime(min);
+  sec = updateTime(sec);
+  document.getElementById("clock").innerText = hour + " : " + min + " : " + sec + " " + midday; /* adding time to the div */
+    var t = setTimeout(currentTime, 1000); /* setting timer */
+}
+
+function updateTime(k) { /* appending 0 before time elements if less than 10 */
+  if (k < 10) {
+    return "0" + k;
+  }
+  else {
+    return k;
+  }
+}
+
+function currentDate() {
+  var today = new Date();
+  var dd = String(today.getDate()).padStart(2, '0');
+  var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+  var yyyy = today.getFullYear();
+  today = dd + '/' + mm + '/' + yyyy;
+  document.getElementById("date").innerText= today;
+}
+currentDate();
+currentTime();
---
+++ script/main.js
@@ -0,0 +1,18 @@
+let navBarStauts = false;
+let toggleNav = function() {
+  let getSideBar = document.querySelector(".nav-side");
+  let getSideBarUL = document.querySelector(".nav-side ul");
+
+
+  if (navBarStauts === false){
+    getSideBarUL.style.visibility="visible";
+    getSideBar.style.width="250px";
+    navBarStauts = true;
+  }
+  else if (navBarStauts === true) {
+    getSideBarUL.style.visibility="hidden";
+    getSideBar.style.width="60px";
+    navBarStauts = false;
+
+  }
+}
